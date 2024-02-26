589bb73bcaafa1c9e568a61e593293eda7fda036
Luka Vukovic <vukovicluka33@gmail.com>
2024-02-26 21:43:16 +0100

    Homepage searchbar + minor css styling

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -2,7 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Product } from './product.model';
 import { ProductService } from '../shared/product.service';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Subscription, take } from 'rxjs';
+import { BehaviorSubject, Observable, Subscription, take } from 'rxjs';
 import { AuthService } from '../auth/auth.service';
 
 @Component({
@@ -13,9 +13,12 @@ import { AuthService } from '../auth/auth.service';
 })
 export class ProductListComponent implements OnInit, OnDestroy {
   products: Product[];
+  filteredProducts: Product[];
   productsSub: Subscription;
   adminSub: Subscription;
   isAdmin = false;
+  private filteredProductsSubject = new BehaviorSubject<Product[]>([]);
+  filteredProducts$: Observable<Product[]> = this.filteredProductsSubject.asObservable();
 
   constructor(
     private productService: ProductService, 
@@ -30,12 +33,10 @@ export class ProductListComponent implements OnInit, OnDestroy {
       if(this.isAdmin){
         this.router.navigate(['admin-dashboard']);
       }
-      else{
-        
-      }
     })
     this.productsSub = this.productService.productsSubject.subscribe(products => {
       this.products = products;
+      this.filteredProductsSubject.next(products);
     });
     this.productService.getProducts();
   }
@@ -51,5 +52,12 @@ export class ProductListComponent implements OnInit, OnDestroy {
 
   loadProduct(id: number){
     this.router.navigate([id], {relativeTo: this.route})
+  }
+
+  searchByKeyword(keyword: string){
+    const filteredProducts = this.products.filter(product => 
+      product.name.toLowerCase().includes(keyword.toLowerCase())
+    );
+    this.filteredProductsSubject.next(filteredProducts.slice());
   }
 }
